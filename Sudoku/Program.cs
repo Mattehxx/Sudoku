7f64a299f922890c72075ecfe0985760d7e89e1b
Sapo125 <Dsaporiti072@gmail.com>
2024-01-25 17:58:15 +0100

    added checks for columns/rows/boxes and EditTable

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -42,10 +42,160 @@ namespace Sudoku
 
         public static bool EditTable (int x, int y, int value)
         {
-            //Edit the temporary table
+            if (CheckCoordinates(x, y) && CheckValue(value) && IsEditable(x, y))
+            {
+                editableTable[x, y] = value;
+                return true;
+            } 
             return false;
         }
 
+        public static bool CheckColumns(int x, int y, int value)
+        {
+            if (editableTable[y, x] == value)
+                return true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (editableTable[y, i] == value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool CheckRows(int x, int y, int value)
+        {
+            if (editableTable[y, x] == value)
+                return true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (editableTable[i, x] == value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool CheckBoxes(int x, int y, int value)
+        {
+            if (editableTable[y, x] == value)
+                return true;
+            if ((x >= 0 && x <= 2) && (y >= 0 && y <= 2))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    if (editableTable[i, j] == value)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if ((x >= 3 && x <= 5) && (y >= 0 && y <= 2))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 3; j < 6; j++)
+                        if (editableTable[i, j] == value)
+                        {
+                            return false;
+                        }
+                }
+            }
+
+            if ((x >= 6 && x <= 8) && (y >= 0 && y <= 2))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 6; j < 9; j++)
+                        if (editableTable[i, j] == value)
+                        {
+                            return false;
+                        }
+                }
+            }
+
+            if ((x >= 0 && x <= 2) && (y >= 3 && y <= 5))
+            {
+                for (int i = 3; i < 6; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                        if (editableTable[i, j] == value)
+                        {
+                            return false;
+                        }
+                }
+            }
+            if ((x >= 0 && x <= 2) && (y >= 3 && y <= 5))
+            {
+                for (int i = 3; i < 6; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                        if (editableTable[i, j] == value)
+                        {
+                            return false;
+                        }
+                }
+            }
+            if ((x >= 3 && x <= 5) && (y >= 3 && y <= 5))
+            {
+                for (int i = 3; i < 6; i++)
+                {
+                    for (int j = 3; j < 6; j++)
+                        if (editableTable[i, j] == value)
+                        {
+                            return false;
+                        }
+                }
+            }
+            if ((x >= 6 && x <= 8) && (y >= 3 && y <= 5))
+            {
+                for (int i = 3; i < 6; i++)
+                {
+                    for (int j = 6; j < 9; j++)
+                        if (editableTable[i, j] == value)
+                        {
+                            return false;
+                        }
+                }
+            }
+            if ((x >= 0 && x <= 2) && (y >= 6 && y <= 8))
+            {
+                for (int i = 6; i < 9; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                        if (editableTable[i, j] == value)
+                        {
+                            return false;
+                        }
+                }
+            }
+            if ((x >= 3 && x <= 5) && (y >= 6 && y <= 8))
+            {
+                for (int i = 6; i < 9; i++)
+                {
+                    for (int j = 3; j < 6; j++)
+                        if (editableTable[i, j] == value)
+                        {
+                            return false;
+                        }
+                }
+            }
+            if ((x >= 6 && x <= 8) && (y >= 6 && y <= 8))
+            {
+                for (int i = 6; i < 9; i++)
+                {
+                    for (int j = 6; j < 9; j++)
+                        if (editableTable[i, j] == value)
+                        {
+                            return false;
+                        }
+                }
+            }
+        }
         public static string PrintTable ()
         {
             //Print the editable table
