2abd7afd8190629e54b85eed8bb5779a6f5a2861
Matteo Rovellini <teorove04@gmail.com>
2024-01-25 17:13:06 +0100

    Project structure

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -8,19 +8,53 @@ namespace Sudoku
 {
     internal class Program
     {
-        public int[,] table = { 
+        public static readonly int[,] table = { 
             { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
             { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
             { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
             { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
-            {4, 0, 0, 8, 0, 3, 0, 0, 1 },
-            {7, 0, 0, 0, 2, 0, 0, 0, 6 },
-            {0, 6, 0, 0, 0, 0, 2, 8, 0 },
-            {0, 0, 0, 4, 1, 9, 0, 0, 5 },
-            {0, 0, 0, 0, 8, 0, 0, 7, 9 }
+            { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+            { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+            { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+            { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+            { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
         };
+
+        public static int[,] editableTable = (int[,])table.Clone();
+
+        public static bool CheckCoordinates(int x, int y)
+        {
+            //0 - 8
+            return false;
+        }
+
+        public static bool IsEditable (int x, int y)
+        {
+            //value[x][y] == 0
+            return false;
+        }
+
+        public static bool CheckValue (int value)
+        {
+            //1 - 9
+            return false;
+        }
+
+        public static bool EditTable (int x, int y, int value)
+        {
+            //Edit the temporary table
+            return false;
+        }
+
+        public static string PrintTable ()
+        {
+            //Print the editable table
+            return "";
+        }
+ 
         static void Main(string[] args)
         {
+
         }
     }
 }
